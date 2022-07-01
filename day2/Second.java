2d4ef1a852e55687419a8e205b3376d11cbed67e
Gagan Indoriya <gaganindoriya0@gmail.com>
2022-07-01 13:58:02 -0700

    Learn bitwise operators

---
+++ day2/Second.java
@@ -0,0 +1,14 @@
+public class Second {
+    public static void main(String args[]){
+        
+        
+        boolean a=true;
+        boolean b=false;
+        if(a^b){
+            System.out.println("Yes");
+        }else{
+            System.out.println("NO");
+        }
+        System.out.println(~4);
+    }
+}
